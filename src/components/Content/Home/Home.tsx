56961e004582f8236dfdb6f12be3b64c5af73431
marvintomizawa <marvintomizawa@hotmail.com>
2021-02-02 17:54:15 -0300

    Added view link to curriculum

--- src/components/Content/Home/Home.tsx
+++ src/components/Content/Home/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import CurriculumDownload from "./CurriculumDownload";
 import { ContentWrapper } from "../ContentStyle";
 
 const Home: React.FunctionComponent = () => {
@@ -8,6 +9,7 @@ const Home: React.FunctionComponent = () => {
   return (
     <ContentWrapper>
       <h1>{t("content.home.title")}</h1>
+      <CurriculumDownload />
     </ContentWrapper>
   );
 };
