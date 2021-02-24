4518007e0ada1f828c292fbe4e8d63f7a0383fa0
marvintomizawa <marvintomizawa@hotmail.com>
2021-02-24 15:03:09 -0300

    fix: removed left scroll

--- src/components/Ui/common/CustomScroll.tsx
+++ src/components/Ui/common/CustomScroll.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { getTheme } from "../Variables";
 
 interface CustomScrollProps {
-  leftScroll?: boolean;
   scrollColor?: string;
   scrollHoveredColor?: string;
   className?: string;
@@ -13,33 +12,23 @@ const CustomScrollBar: React.FunctionComponent<CustomScrollProps> = (props) => {
   return (
     <CustomScrollBarWrapper
       className={props.className}
-      leftScroll={props.leftScroll}
       scrollColor={props.scrollColor}
       scrollHoveredColor={props.scrollHoveredColor}
     >
-      <Content>{props.children}</Content>
+      {props.children}
     </CustomScrollBarWrapper>
   );
 };
 
 export default CustomScrollBar;
 
-const Content = styled.div`
-  direction: ltr;
-  height: 100%;
-  width: 100%;
-`;
-
 const CustomScrollBarWrapper = styled.div.attrs<CustomScrollProps>((props) => ({
-  leftScroll: props.leftScroll || false,
   scrollColor: props.scrollColor,
   scrollHoveredColor: props.scrollHoveredColor || props.scrollColor,
 }))<CustomScrollProps>`
   height: 100%;
   width: 100%;
   overflow: auto;
-
-  ${(props) => props.leftScroll && "direction: rtl;"}
 
   ::-webkit-scrollbar {
     height: 0.5rem;
